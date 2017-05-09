2e4c934701e5bf6ae9a1326628ef8665c77c665e
Daniel Brandt <dbbrandt@gmail.com>
2017-05-08 23:40:05 -0400

    Added FAE admin routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
 
+  resources :goals do
+    resources :interactions
+  end
+
+  resources :interactions do
+    resources :contents
+  end
+
   namespace :admin do
+    resources :contents
+    resources :interactions
+    resources :goals do
+      resource :interactions
+    end
   end
   # mount Fae below your admin namespec
   mount Fae::Engine => '/admin'
