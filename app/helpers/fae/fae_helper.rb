24076c6f0f5380dade35b39acfa02060a84fc50d
Daniel Brandt <dbbrandt@gmail.com>
2017-05-10 16:15:10 -0400

    Add view helper buttons for add and edit.

--- app/helpers/fae/fae_helper.rb
+++ app/helpers/fae/fae_helper.rb
@@ -4,11 +4,24 @@ module Fae
     def fae_add_button(item, add_path = nil, *custom_attrs)
       return if item.blank?
       add_path ||= polymorphic_path([main_app, fae_scope, item.try(:fae_parent), item])
-      attrs = { }
+      #attrs = { method: :delete, title: 'Delete', class: 'js-tooltip table-action', data: { confirm: t('fae.delete_confirmation') } }
+      attrs = { title: 'Add', class: 'js-tooltip table-action' }
       attrs.deep_merge!(custom_attrs[0]) if custom_attrs.present?
       link_to add_path, attrs do
-        concat content_tag :i, nil, class: 'add-button', style: 'padding: 5px 3px 5px 10px;'
+        concat content_tag :i, nil, class: 'icon-plus'
       end
     end
+
+    def fae_edit_button(item, edit_path = nil, *custom_attrs)
+      return if item.blank?
+      edit_path ||= polymorphic_path([main_app, fae_scope, item.try(:fae_parent), item])
+      attrs = { title: 'Edit', class: 'js-tooltip table-action' }
+      attrs.deep_merge!(custom_attrs[0]) if custom_attrs.present?
+      link_to edit_path, attrs do
+        concat content_tag :i, nil, class: 'icon-edit'
+      end
+    end
+
+
   end
 end
