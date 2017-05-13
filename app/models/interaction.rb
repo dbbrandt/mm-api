3722d19b0b9a6e6aff0764ab1889f4b5031b6d24
Daniel Brandt <dbbrandt@gmail.com>
2017-05-12 22:04:10 -0400

    Validate answer_type

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -10,6 +10,9 @@ class Interaction < ApplicationRecord
   #acts_as_list add_new_at: :top
   default_scope { order('updated_at DESC') }
 
+  validates_presence_of :answer_type
+  validates_inclusion_of :answer_type, in: TYPES
+
   belongs_to :goal
   has_many :contents
 end
