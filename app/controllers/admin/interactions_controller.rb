4871c21251e7c8ff50064edccc1b18eee0932109
Daniel Brandt <dbbrandt@gmail.com>
2017-05-12 22:12:30 -0400

    Set the @goal_id value for filtering index.

--- app/controllers/admin/interactions_controller.rb
+++ app/controllers/admin/interactions_controller.rb
@@ -1,28 +1,12 @@
 module Admin
-  class InteractionsController < Fae::BaseController
+  class InteractionsController < FaeBaseController
+    before_action :set_goal
 
-    def show
-      @items = Goal.find_by_id(params[:goal_id]).interactions
-      @goal = @items.first.goal
-    end
-
-    def new
-      @goal = Goal.find_by_id(params[:goal_id])
-      super
-    end
-
-    def create
-      return create_from_existing(params[:from_existing]) if params[:from_existing].present?
-
-      @item = @klass.new(item_params)
-
-      if @item.save
-        #redirect_to @index_path, notice: t('fae.save_notice')
-        redirect_to admin_goal_interactions_path(goal_id: @item.goal_id), notice: t('fae.save_notice')
+    def index
+      if @goal
+        @items = Interaction.where(goal_id: @goal.id)
       else
-        build_assets
-        flash[:alert] = t('fae.save_error')
-        render action: 'new'
+        @items = Interaction.all
       end
     end
 
@@ -33,6 +17,10 @@ module Admin
           render :text => '<tbody aria-live="polite" aria-relevant="all"><tr id="interaction_5" role="row"><td><a href="/admin/interactions/5/edit">New Jonah Hill</a></td><td>05/09/17</td><td><a title="Delete" class="js-tooltip table-action tooltip-parent" data-confirm="Are you sure you want to permanently delete this item?" rel="nofollow" data-method="delete" href="/admin/interactions/5"><span class="tooltip">Delete</span><i class="icon-trash"></i></a></td></tr><tr id="interaction_6" role="row"><td><a href="/admin/interactions/6/edit">Martin Sheen</a></td><td>05/09/17</td><td><a title="Delete" class="js-tooltip table-action tooltip-parent" data-confirm="Are you sure you want to permanently delete this item?" rel="nofollow" data-method="delete" href="/admin/interactions/6"><span class="tooltip">Delete</span><i class="icon-trash"></i></a></td></tr><tr id="interaction_4" role="row"><td><a href="/admin/interactions/4/edit">Meryl Streep</a></td><td>05/08/17</td><td><a title="Delete" class="js-tooltip table-action tooltip-parent" data-confirm="Are you sure you want to permanently delete this item?" rel="nofollow" data-method="delete" href="/admin/interactions/4"><span class="tooltip">Delete</span><i class="icon-trash"></i></a></td></tr><tr id="interaction_3" role="row"><td><a href="/admin/interactions/3/edit">Tom Hanks</a></td><td>05/08/17</td><td><a title="Delete" class="js-tooltip table-action tooltip-parent" data-confirm="Are you sure you want to permanently delete this item?" rel="nofollow" data-method="delete" href="/admin/interactions/3"><span class="tooltip">Delete</span><i class="icon-trash"></i></a></td></tr></tbody>'
         }
       end
+    end
+
+    def set_goal
+      @goal = Goal.find_by_id(params[:goal_id])
     end
 
   end
