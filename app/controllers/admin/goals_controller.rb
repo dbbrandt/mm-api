d2c1f6f329aa484d8190ec85f48f522b30f0834c
Daniel Brandt <dbbrandt@gmail.com>
2017-05-12 22:03:30 -0400

    Removed unused method.

--- app/controllers/admin/goals_controller.rb
+++ app/controllers/admin/goals_controller.rb
@@ -1,10 +1,5 @@
 module Admin
-  class GoalsController < Fae::BaseController
-
-    def interactions
-      @items = Interaction.where(goal_id: params[:id])
-      @goal = @item.goal
-    end
+  class GoalsController < FaeBaseController
 
     private
 
