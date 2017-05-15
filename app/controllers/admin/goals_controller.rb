9fbf42b782713de37414ab2cf6b1e008f507393a
Daniel Brandt <dbbrandt@gmail.com>
2017-05-15 07:54:08 -0400

    Remove base classes used to override FAE::BaseController index path.
    Replaced with cookie method..soooo much simpler..:(

--- app/controllers/admin/goals_controller.rb
+++ app/controllers/admin/goals_controller.rb
@@ -1,11 +1,9 @@
 module Admin
-  class GoalsController < AdminBaseController
-
+  class GoalsController < Fae::BaseController
     private
 
     def build_assets
       @item.build_hero_image if @item.hero_image.blank?
     end
-
   end
 end
