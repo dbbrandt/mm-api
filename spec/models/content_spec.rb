50e1199ec6b64160634d9328628e00b6ea1a6cbe
Daniel Brandt <dbbrandt@gmail.com>
2017-06-03 16:54:04 -0400

    Fix incorrect attribute name throught for title.

--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -6,16 +6,18 @@ RSpec.describe Content, type: :model do
   it { is_expected.to belong_to(:interaction) }
 
   # Validation tests
-  # ensure columns name and type are present before saving
+  # ensure columns title and content_type are present and valid before saving
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_inclusion_of(:content_type).in_array(Content::TYPES) }
 
+  # Stimulus must be present if there is no copy for the interaction to be valid
   it "is invalid without stimulus if copy is missing" do
     content = Content.new(:title => 'title', :content_type => 'Prompt')
 
     content.validate
     expect(content.errors[:stimulus]).to include("can't be blank")
 
+    # Make sure the content is valid if copy is added.
     content.copy = 'Copy'
     expect(content).to be_valid
   end
