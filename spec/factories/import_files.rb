c4aa089335a7e0dee2ef8c1f50414109650e1bd7
Daniel Brandt <dbbrandt@gmail.com>
2017-07-10 15:30:29 -0400

    Add missing goal to factory

--- spec/factories/import_files.rb
+++ spec/factories/import_files.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
   factory :import_file do
     title { Faker::Lorem.word }
     json_data { "[{\"title\": \"#{Faker::Lorem.word}\",\"answer_type\": \"ShortAnswer\"}]" }
+    goal { }
   end
 end
