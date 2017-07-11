388d5c15d4ece0f369199f58fabb6e9078e9a5f5
Daniel Brandt <dbbrandt@gmail.com>
2017-07-10 22:27:13 -0400

    Add retrun value to support controller error handling.

--- app/models/import_file.rb
+++ app/models/import_file.rb
@@ -14,25 +14,33 @@ class ImportFile < ApplicationRecord
   has_many :import_rows, dependent: :destroy
 
   # Create all the import_rows from the json_data
+  # Return true if rows were created
   def create_rows
     # Once rows are created from the json_date they cannot be recreated to avoid possible duplication.
-    return if import_rows.size > 0
+    return false if import_rows.size > 0
     rows = JSON.parse(json_data)
+    row_count = 0
     ActiveRecord::Base.transaction do
       rows.each do |row|
-        new_row = import_rows.create!(title: row["title"], json_data: row.to_json)
-        puts new_row
+        import_rows.create!(title: row["title"], json_data: row.to_json)
+        row_count += 1
       end
     end
+    reload
+    return row_count > 0
   end
 
+  # Delete all rows not associated with an interaction
+  # Return true if all rows deleted.
   def delete_rows
-    return if import_rows.size == 0
+    return true if import_rows.size == 0
     ActiveRecord::Base.transaction do
       import_rows.each do |row|
         row.destroy unless row.interaction
       end
     end
+    reload
+    return import_rows.size == 0
   end
 
 
