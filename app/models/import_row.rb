e71030fa9b4e20ef9f46003a0577d4658c9e5bfc
dbrandt <dbrandt@onekingslane.com>
2017-12-17 21:14:48 -0500

    Add validation for unique title.
    1) Making title unique will allow updates of import rows.
    2) Removed redundant method generated by Rails.

--- app/models/import_row.rb
+++ app/models/import_row.rb
@@ -2,6 +2,7 @@ class ImportRow < ApplicationRecord
   include Fae::BaseModelConcern
   include ActiveModel::Validations
 
+  validates_uniqueness_of :title, scope: :import_file_id
   validates_with ImportRowValidator
 
   def fae_display_field
@@ -11,15 +12,10 @@ class ImportRow < ApplicationRecord
   # order by id for ease of review in sequential import
   default_scope { order('id') }
 
-
   belongs_to :import_file
   has_one :interaction
 
-  def interaction
-    Interaction.find_by_import_row_id(id)
-  end
-
   def json
-    JSON.parse(json_data)
+    @json ||= JSON.parse(json_data)
   end
 end
