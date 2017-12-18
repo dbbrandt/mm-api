e01a8527918180bf1ca5142360de7a1f98849e5b
dbrandt <dbrandt@onekingslane.com>
2017-12-17 21:16:17 -0500

    Improve error messages and streamline code.

--- app/validators/import_row_validator.rb
+++ app/validators/import_row_validator.rb
@@ -1,14 +1,19 @@
 class ImportRowValidator < ActiveModel::Validator
   def validate(record)
 
-    if record.title.blank?
-      record.errors.add(:title, "can't be blank")
-    end
+    #TODO insure title is unique for the import file
+
+    record.errors.add(:title, "can't be blank") if record.title.blank?
 
     json = record.json_data
 
     if json.blank?
       record.errors.add(:json_data, "can't be blank")
+      return
+    end
+
+    if ImportRow.where(import_file_id: record.import_file_id, title: record.title).count > 0
+      record.errors.add(:title, "must be unique. Duplicate value: #{record.title}")
       return
     end
 
@@ -19,22 +24,20 @@ class ImportRowValidator < ActiveModel::Validator
       return
     end
 
-    if row["prompt"].blank?
-      record.errors.add(:prompt, "can't be blank.")
-    end
+
+    record.errors.add(:prompt, "can't be blank: #{record.title}.") if row["prompt"].blank?
 
     if row["answer_type"].blank? or !(Interaction::TYPES.include?(row["answer_type"]))
-      record.errors.add(:answer_type, "must be a valid type (#{Interaction::TYPES.to_s})")
+      record.errors.add(:answer_type, "must be a valid type (#{Interaction::TYPES.to_s}): #{record.title}")
     end
 
     if row["criterion1"].blank? or row["points1"].nil?
-      record.errors.add(:criterion, ": At least one complete criterion (criterion1 and points1) is required.")
+      record.errors.add(:criterion, ": At least one complete criterion (criterion1 and points1) is required: #{record.title}")
     end
 
-    total_points = row["points1"].to_i + row["points2"].to_i + row[:point3].to_i + row[:points4].to_i
-    if total_points == 0
-      record.errors.add(:points, ": At least one criterion is required to have points.")
+    total_points = row["points1"].to_i + row["points2"].to_i + row["points3"].to_i + row["points4"].to_i
+
+    record.errors.add(:points, ": At least one criterion is required to have points: #{record.title}") if total_points == 0
     end
 
-  end
 end
