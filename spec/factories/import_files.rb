5d3f699c47a7fd7be8b57fbc3dded7de42c4310c
dbrandt <dbrandt@onekingslane.com>
2017-12-30 15:34:06 -0500

    Fix import_file factor json data to support additional import_row validaiton added.

--- spec/factories/import_files.rb
+++ spec/factories/import_files.rb
@@ -2,7 +2,7 @@
 FactoryGirl.define do
   factory :import_file do
     title { Faker::Lorem.word }
-    json_data { "[{\"title\": \"#{Faker::Lorem.word}\",\"answer_type\": \"ShortAnswer\"}]" }
+    json_data { "[{\"title\": \"#{Faker::Lorem.word}\",\"answer_type\": \"ShortAnswer\",\"prompt\": \"#{Faker::Lorem.word}\",\"criterion1\": \"#{Faker::Lorem.word}\",\"points1\": \"1\"}]" }
     goal { }
   end
 end
