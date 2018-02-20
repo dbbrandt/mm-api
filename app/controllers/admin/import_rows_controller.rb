b27872a63d087b4200adc5140a7bcb09ca20a5b9
dbrandt <dbrandt@onekingslane.com>
2018-02-19 22:54:27 -0500

    Added missing support for scopped rows to the import file.
    This controller is much like the interation controller which is linked to through the import file.

--- app/controllers/admin/import_rows_controller.rb
+++ app/controllers/admin/import_rows_controller.rb
@@ -1,5 +1,19 @@
 module Admin
   class ImportRowsController < Fae::BaseController
+    before_action :set_import_file
+
+    def index
+      if @import_file
+        @items = ImportRow.where(import_file_id: @import_file.id)
+      else
+        @items = ImportRow.all
+      end
+    end
+
+    def set_import_file
+      cookies[:import_file_id] = params[:import_file_id] if params[:import_file_id]
+      @import_file = ImportFile.find_by_id(cookies[:import_file_id])
+    end
 
   end
 end
