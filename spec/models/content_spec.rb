e16159147ec87cb4398aae7cf152b8bd5329c168
dbrandt <dbrandt@onekingslane.com>
2018-02-23 17:38:42 -0500

    Add required interaction model to creation of content row.

--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -10,9 +10,12 @@ RSpec.describe Content, type: :model do
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_inclusion_of(:content_type).in_array(Content::TYPES) }
 
+  let!(:goal) { create(:goal) }
+  let!(:interaction) { create(:interaction, goal: goal) }
+
   # Stimulus must be present if there is no copy for the interaction to be valid
   it "is invalid without stimulus if copy is missing" do
-    content = Content.new(:title => 'title', :content_type => 'Prompt')
+    content = Content.new(:interaction => interaction, :title => 'title', :content_type => 'Prompt')
 
     content.validate
     expect(content.errors[:stimulus]).to include("can't be blank")
