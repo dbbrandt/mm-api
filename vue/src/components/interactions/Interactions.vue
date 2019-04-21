815312cccc0f2b99c4d3afda69262615c77cf68c
Daniel Brandt <dbbrandt@gmail.com>
2019-04-21 17:01:32 -0400

    Handle focus for input of answer.

--- vue/src/components/interactions/Interactions.vue
+++ vue/src/components/interactions/Interactions.vue
@@ -35,7 +35,7 @@
           {{prompt_copy}}
         </div>
         <div class="response">
-          <input align="left" type="Text" placeholder="answer..." v-model="answer" class="response_text" v-on:keyup.enter="showTitle"/>
+          <input align="left" type="Text" placeholder="answer..." v-model="answer" class="response_text" v-on:keyup.enter="showTitle" ref="answer" v-focus/>
         </div>
         <div v-if="correct_answer">
           <div class="show-button">
@@ -159,6 +159,7 @@
         this.answer = '';
         this.response = '';
         this.position += 1;
+        this.$refs.answer.focus();
         if (this.done) {
           alert(`Completed. Correct: ${this.correct_answers} Result: ${this.percent}% Score = ${this.total_score}`);
           this.resetPosition();
--- vue/src/main.js
+++ vue/src/main.js
@@ -25,6 +25,15 @@ Vue.component('Spinner', Spinner);
 
 Vue.config.productionTip = false;
 
+// Register a global custom directive called `v-focus`
+Vue.directive('focus', {
+  // When the bound element is inserted into the DOM...
+  inserted(el) {
+    // Focus the element
+    el.focus();
+  },
+});
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
