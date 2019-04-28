023e2fa65046aedb7a0a68520570e5d565e14ae4
Daniel Brandt <dbbrandt@gmail.com>
2019-04-28 18:29:44 -0400

    Fix Focus After Review Submitted
    Return focus to answer.

--- vue/src/components/interactions/Interactions.vue
+++ vue/src/components/interactions/Interactions.vue
@@ -5,10 +5,27 @@
   <div v-else>
     <template v-if="interactions">
       <div class="refresh-form">
-        as of <DateTime :value='loadTime' :timeIfToday='true'/>
+        <div>as of <DateTime :value='loadTime' :timeIfToday='true'/></div>
+        <div class="refresh">
         <v-btn flat v-on:click="loadInteractions" class="refresh-btn">refresh</v-btn>
-        <v-text-field :full-width=false v-model="max_interactions" label="Questions" placeholder="questions?" outline />
-        Interactions Answered: {{position}} / Correct: {{ correct_answers }} / {{ percent }}%
+        </div>
+        <div>
+          <div  class="questions">
+            Questions:
+          </div>
+          <div class="question_input">
+            <input align="left" type="Text" placeholder="Questions" v-model="max_interactions" class="questions_text"/>
+          </div>
+        </div>
+        <div class="questions">
+          Interactions Answered: {{score_count}}
+        </div>
+        <div>
+          Correct: {{ correct_answers }} / {{ percent }}%
+        </div>
+        <div>
+          Score: {{ total_score }}%
+        </div>
       </div>
       <div class="form">
         <div v-if="image_url">
@@ -18,19 +35,23 @@
           {{prompt_copy}}
         </div>
         <div class="response">
-          <v-text-field :full-width=false v-model="answer" label="Answer" placeholder="Answer?" outline/>
+          <input align="left" type="Text" placeholder="answer..." v-model="answer" class="response_text" v-on:keydown.enter="showTitle" ref="answer" v-focus/>
         </div>
         <div v-if="correct_answer">
           <div class="show-button">
             <v-btn v-on:click="showTitle" class="vbutton">Show</v-btn>
           </div>
           <div v-if="show_title">
+            <div v-if="isLoadingAnswer">
+              <Spinner msg='Checking Answer'/>
+            </div>
+            <div v-else>
             <div class="answer">
               {{correct_answer}} - {{correct_text}} ({{ score }})
             </div>
             <div>
-              <v-btn v-on:click="correctNext" class="vbutton">Correct</v-btn>
-              <v-btn v-on:click="nextInteraction" class="vbutton">Nope</v-btn>
+                <v-btn v-on:click="correctNext" class="vbutton" ref="correct">Correct</v-btn>
+                <v-btn v-on:click="nextInteraction" class="vbutton" ref="incorrect">Nope</v-btn>
               <v-btn v-on:click="nextInteraction" class="vbutton">Skip</v-btn>
             </div>
             <div class="copy">
@@ -38,6 +59,7 @@
             </div>
           </div>
         </div>
+      </div>
       </div>
     </template>
   </div>
@@ -54,13 +76,19 @@
         interactions: null,
         isLoading: false,
         show_title: false,
+        isLoadingAnswer: false,
         loadTime: null,
+        correctThreshold: 0.85,
         correct_answers: 0,
+        aggregate_score: 0,
+        score_count: 0,
         position: 0,
         goal: null,
         answer: '',
-        correct: '',
+        round: 0,
+        review_correct: false,
         result: null,
+        review: null,
       };
     },
     mounted() {
@@ -90,27 +118,31 @@
         return this.interaction.criterion[0].copy;
       },
       percent() {
-        return (this.position > 0) ? 100 * (this.correct_answers / this.position) : 0;
+        return (this.position > 0) ? Math.round(100 * (this.correct_answers / this.score_count)) : 0;
       },
       done() {
-        return this.position >= this.total - 1;
+        return this.position >= this.total;
       },
       score() {
-        return this.result ? this.result.score : 0;
+        return this.result ? Math.round(this.result.score * 100) : 0;
+      },
+      correct() {
+        return this.result ? this.result.correct : false;
+      },
+      total_score() {
+        const totalScore = Math.round(this.aggregate_score / this.score_count);
+        return this.score_count > 0 ? totalScore : 0;
       },
       correct_text() {
-        if (this.result) {
-          this.correct = this.result.correct ? 'Correct!' : 'Incorrect.';
-        } else {
-          this.correct = 'Unavailable..';
-        }
-        return this.correct;
+        return this.result && this.result.correct ? 'Correct!' : 'Incorrect.';
       },
     },
     methods: {
       resetPosition() {
         this.position = 0;
         this.correct_answers = 0;
+        this.aggregate_score = 0;
+        this.score_count = 0;
         this.show_title = false;
       },
       loadInteractions() {
@@ -127,32 +159,53 @@
       },
       correctNext() {
         this.correct_answers += 1;
+        this.review_correct = true;
         this.nextInteraction();
       },
       nextInteraction() {
+        this.submit_review();
         this.show_title = false;
+        this.review_correct = false;
         this.answer = '';
         this.response = '';
+        this.position += 1;
+        this.$refs.answer.focus();
         if (this.done) {
-          this.position += 1;
-          alert(`Completed. Correct: ${this.correct_answers} Result: ${this.percent}%`);
+          alert(`Completed. Correct: ${this.correct_answers} Result: ${this.percent}% Score = ${this.total_score}`);
           this.resetPosition();
-        } else {
-          this.position += 1;
         }
       },
       checkInteraction() {
-        this.isLoading = true;
+        // this.isLoadingAnswer = true;
         api.interactions.check(this.goal, this.interaction_id, this.answer)
           .then((response) => {
             this.result = response;
+            this.aggregate_score += this.score;
+            this.score_count += 1;
           }).finally(() => {
-            this.isLoading = false;
+            this.isLoadingAnswer = false;
+            this.$nextTick(() => {
+              if (this.score >= this.correctThreshold) {
+                this.$refs.correct.$el.focus();
+              } else {
+                this.$refs.incorrect.$el.focus();
+              }
+            });
+          });
+      },
+      submit_review() {
+        api.interactions.review(this.goal, this.interaction_id, this.round, this.answer, this.score,
+                                this.correct, this.review_correct)
+          .then((response) => {
+            this.review = response;
+            this.round = this.review.round;
           });
       },
       showTitle() {
-        this.checkInteraction();
-        this.show_title = true;
+        if (!this.show_title) {
+          this.checkInteraction();
+          this.show_title = true;
+        }
       },
     },
   };
@@ -171,17 +224,35 @@
     padding-top: 40px;
     width: 300px;
   }
+
   .prompt {
     font-size: 30px;
     padding: 40px;
   }
 
-  .answer {
+  .questions {
+    float: left;
+  }
+
+  .question_input input {
+    width: 30px;
+    border: 1px solid #b1b1b1;
+    padding-left: 5px;
+  }
+
+  .question_text {
     font-size: 20px;
-    padding: 20px;
+  }
+
+  .response_text {
+    font-size: 20px;
+    padding-left: 5px;
+    border: 1px solid #b1b1b1;
   }
 
   .response {
+    padding-top: 20px;
+    padding-bottom: 20px;
     margin: auto;
     width: 33%;
   }
@@ -194,6 +265,15 @@
   }
 
   .show-button  {
+    width: 100%;
+  }
+
+  .refresh {
+    height: 35px;
+  }
+
+  .refresh-btn {
+    text-align: left;
     width: 100%;
   }
 
@@ -229,10 +309,23 @@
       font-size: 30px;
     }
 
+    .refresh {
+      height: 35px;
+      padding-bottom: 60px;
+    }
+
     .refresh-form {
       padding-top: 40px;
       float: left;
       text-align: left;
+    }
+
+    .question_text {
+      font-size: 40px;
+    }
+
+    .response_text {
+      font-size: 40px;
     }
 
     .input-group .input-group__input {
